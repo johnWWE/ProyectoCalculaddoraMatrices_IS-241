f0cb59656a30f4af97f439ad522deeede5b749d3
Emerson Elvis <121317662+EmersonElvis@users.noreply.github.com>
2023-01-28 22:42:05 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-def CrearMatriz(Fila,Columna):
+def IngresarElementoMatriz(Fila,Columna):
     Matriz = []
     for i in range(Fila):
-        Fila_x=[]
+        Fila_x = []
         for j in range(Columna):
             while True:
                 try:
-                    dato = float(input("Elemento (%d,%d): " %(i,j)))
-                    break
+                    Elemento = float(input("Elemento (%d,%d): " %(i,j)))
                 except:
                     continue
-            Fila_x.append(dato)
+                break
+            Fila_x.append(Elemento)
         Matriz.append(Fila_x)
     return Matriz
 
@@ -23,38 +23,32 @@ def Pedir_ValidarDatoEntero(FilaColumna):
             print("¡¡Digite bien!!")
             continue
         return fila_columna
- 
-def ImprimirMatriz(matriz):
-    for linea in matriz:
-        for elemento in linea:
-            print(elemento, end=" ")
-        print()  
 
 def ProductoMatrices():
-    print("\n\tProducto para dos matrices")
-    Fila = "Fila" 
-    Columna = "Columna"
+    print("\n\tMultiplicación de dos Matrices")
+    Fila = "Fila" ; Columna = "Columna"
     print("\nPara el primer Matriz")
-    Filax = Pedir_ValidarDatoEntero(Fila)
-    Columnax = Pedir_ValidarDatoEntero(Columna)
+    Filax = Pedir_ValidarDatoEntero(Fila) ; Columnax = Pedir_ValidarDatoEntero(Columna)
     print("\nPara el segundo Matriz")
-    Filay = Pedir_ValidarDatoEntero(Fila)
-    Columnay = Pedir_ValidarDatoEntero(Columna) 
+    Filay = Pedir_ValidarDatoEntero(Fila) ; Columnay = Pedir_ValidarDatoEntero(Columna) 
     if Columnax == Filay:
-        print("Llenando primer Matriz")
-        MatrizA=CrearMatriz(Filax,Columnax)
-        print("Llenando segundo  Matriz")
-        MatrizB=CrearMatriz(Filax,Columnay)
+        print("Llenando primer Matriz...")
+        MatrizA=IngresarElementoMatriz(Filax,Columnax) ; Matriz1 = np.array(MatrizA)
+        print("Llenando segundo Matriz...")
+        MatrizB=IngresarElementoMatriz(Filay,Columnay) ; Matriz2 = np.array(MatrizB)
         Producto =[]
         for i in range (len(MatrizA)):
             Producto.append([0]*len(MatrizB))
             for j in range (len(MatrizB[0])):
                 for k in range (len(MatrizB)):
                     Producto[i][j] += MatrizA[i][k]*MatrizB[k][j]
-        print("Su Producto de las matrices es: ")
-        ImprimirMatriz(Producto)
+        resultado = np.array(Producto)
+        print("\n\tMatriz A\n", Matriz1)
+        print("\n\tMatriz B\n", Matriz2)
+        print("\nSu Producto de las matrices es:")
+        print(resultado)
     else:
-        print("Recuerde la multiplicación entre matrices debe ser mxn * nxp")
+        print("Recuerde la multiplicación entre dos matrices debe ser mxn * nxp")
 
 #Menú General
 while True:
@@ -69,7 +63,7 @@ while True:
     [0] SALIR """)
     Opcion=input("--->").lower().replace(" ", "")
     listaOpciones=["P", "S", "R","I","D","0","p","s","r","i","d", "PRODUCTO", "SUMA", "RESTA", "INVERSA", "DETERMINANTE",
-                   "SALIR","producto","suma","resta","inversa","determinante","salir"]
+                   "producto","suma","resta","inversa","determinante"]
     while Opcion in listaOpciones:
         if Opcion =="P" or Opcion=="p" or Opcion=="PRODUCTO" or Opcion=="producto":
             ProductoMatrices()
@@ -86,12 +80,10 @@ while True:
         elif Opcion=="D" or Opcion=="d" or Opcion=="DETERMINANTE" or Opcion=="determinante":
             #determinantematrices()
             break
-        else:
-            if Opcion=="0":
-                break
-    if Opcion=="0":
-        break
-    else:
-        if Opcion not in listaOpciones:
-            print("\nDigite Opción correcta")
+    if Opcion not in listaOpciones:
+            print("\n¡¡Digite Opción correcta!!")
             continue
+    else:
+        if Opcion=="0" or Opcion== "salir" or Opcion == "OPCION":
+            break
+print("Fin del programa")
